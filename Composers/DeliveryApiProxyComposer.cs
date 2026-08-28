67e84f006ef57095ef94b32ab720150fd5e70c9d
SzymonBogunia <s61273@student.ubb.edu.pl>
2026-08-28 20:36:04 +0200

    fix/secrets + content update

---
+++ Composers/DeliveryApiProxyComposer.cs
@@ -0,0 +1,16 @@
+﻿using Umbraco.Cms.Core.Composing;
+
+namespace AutomotiveInfo.Composers;
+
+public class DeliveryApiProxyComposer : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        builder.Services.AddHttpClient("DeliveryApiProxy", (sp, client) =>
+        {
+            var config = sp.GetRequiredService<IConfiguration>();
+            var baseUrl = config["App:SelfUrl"] ?? "https://localhost:44328";
+            client.BaseAddress = new Uri($"{baseUrl}/umbraco/delivery/api/v2/");
+        });
+    }
+}
